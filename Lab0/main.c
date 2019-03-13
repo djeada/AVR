2eab33e6df7dbbb64d941e88fdd666cad7b33b34
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-03-13 18:58:56 +0000

    Add files via upload

---
+++ Lab0/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: 20-02-2019
+ *      Author: psm8
+ */
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(){
+
+	DDRA = 0b11111111;
+
+	while(1){
+
+		PORTA ^= 0xFF;
+		_delay_ms (500);
+	}
+
+	return 0;
+}
