28dfc65b6f350cfe280b16d8d057e7b7ed22f56c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-12-07 15:57:52 +0000

    kali update

---
+++ proba.sh
@@ -0,0 +1,9 @@
+#Eerytime you open this shit
+#Author: Adam Djellouli, 28.10.2019 
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install pluma
+sudo apt-get install avrdude
+sudo apt-get install gcc-avr binutils-avr avr-libc
+sudo apt-get install gdb-avr
